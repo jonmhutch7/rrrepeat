6daff310add1b3622f3f277ad5d3d3e585123b91
Jon Hutchison <jonmhutch@mac.com>
2017-05-29 10:41:31 -0400

    begin partial for list articles

--- music_blog_theme/index.php
+++ music_blog_theme/index.php
@@ -2,41 +2,8 @@
 
 <div id="inner-content" class="wrap cf">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-	<article class="home-article" role="article">
-		<?php 
-			$album_art= get_field('album_art');
-		?>
-		<header class="article-header with-background" style="background-image: url('<?php echo $album_art ?>')">
-			<div class="max-width-container">
-				<span class="date"><?php the_date(); ?></span>
-				
-				<h1>
-					<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
-						<?php the_title(); ?>
-					</a>
-				</h1>
-
-				<div class="music-embed"><?php
-						$platform = get_field('embed_from');
-						if ($platform == 'Soundcloud') {
-							echo get_field('home_page_embed');
-						} else {
-							echo get_field('embed_code');
-						}
-					?></div>
-			</div>
-		</header>
-
-		<section class="article-body home-page">
-			<?php the_excerpt(); ?>
-		</section>
-
-		<div class="continue">
-			<a href="<?php the_permalink() ?>">Continue Reading</a>
-		</div>
-
-	</article>
+	
+		<?php get_template_part( 'list', 'article' ); ?>
 
 	<?php endwhile; ?>
 
